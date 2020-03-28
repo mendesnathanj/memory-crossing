117dc9782ffa7ea17df75e740ef791d6b3c7dbc8
Nathan <mendesnathanj@gmail.com>
2020-03-28 13:45:06 -0700

    Add help modal

---
+++ src/game/modal.js
@@ -0,0 +1,42 @@
+import { createElementFromHTML } from '../utils/utils';
+
+class Modal {
+  html() {
+    const container = document.createElement('div');
+    container.id = 'modal-container';
+    container.classList.add('modal-container');
+
+    const modal = document.createElement('div');
+    modal.id = 'modal';
+    modal.classList.add('modal');
+
+    const content = document.createElement('div');
+    content.classList.add('modal-content');
+
+    const close = createElementFromHTML('<i id="modal-close" class="modal-close fas fa-times"></i>');
+    close.addEventListener('click', () => container.classList.toggle('open'));
+
+    content.appendChild(close);
+
+    const header = document.createElement('h1');
+    header.innerText = 'How to Play';
+    content.appendChild(header);
+
+    const p1 = document.createElement('p');
+    p1.innerText = "Flip cards over by clicking on them two at a time.\nIf those cards are a pair, then you will earn bells (points) and they will stay up. If not, then they will go back down.\nKeep flipping until you find all the matching pairs before time runs out!";
+
+    content.appendChild(p1);
+
+    const p2 = document.createElement('p');
+    p2.innerText = "Earn bonus points by flipping matching pairs over consecutively.\nYour score will skyrocket the higher your streak goes!";
+
+    content.appendChild(p2);
+
+    modal.appendChild(content);
+    container.appendChild(modal);
+    return container;
+  }
+}
+
+
+export default Modal;
